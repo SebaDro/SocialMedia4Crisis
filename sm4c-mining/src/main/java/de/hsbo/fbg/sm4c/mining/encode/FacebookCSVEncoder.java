cc7adbd595b647d40c6d7037161bcd956b86f94f
SebaDro <sebastian.drost@hs-bochum.de>
2017-06-21 14:10:20 +0200

    rename FacebookEncoder

---
+++ sm4c-mining/src/main/java/de/hsbo/fbg/sm4c/mining/encode/FacebookCSVEncoder.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.hsbo.fbg.sm4c.mining.encode;
+
+import facebook4j.Group;
+import java.util.List;
+
+/**
+ *
+ * @author Sebastian Drost
+ */
+public class FacebookCSVEncoder {
+
+    public String createGroupCSV(List<Group> facebookGroups) {
+        StringBuilder builder = new StringBuilder("id;name;update_time");
+        builder.append(System.getProperty("line.separator"));
+        facebookGroups.forEach(g -> {
+            builder.append(g.getId());
+            builder.append(";");
+            builder.append(g.getName());
+            builder.append(";");
+            builder.append(g.getUpdatedTime());
+            builder.append(System.getProperty("line.separator"));
+        });
+        builder.setLength(builder.length()-1);
+        String result = builder.toString();
+        return result;
+    }
+}

c139b9aa9d25b351cc10e695fac549a303fabe8f
SebaDro <sebastian.drost@hs-bochum.de>
2017-06-23 21:48:49 +0200

    add module for text classifying

---
+++ sm4c-classifier/src/main/java/de/hsbo/fbg/sm4c/tagging/filter/StopWordRemover.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.hsbo.fbg.sm4c.tagging.filter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Sebastian Drost
+ */
+public class StopWordRemover {
+
+
+    public StopWordRemover() {
+    }
+
+    public String removeStopWords(String text, String[] stopWords) {
+        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String removeStopWords(String text, List<String> stopWords) {
+        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+    }
+
+}

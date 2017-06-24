754bb51c6fa771e8ec36d5e92c4b41b49334b1d6
SebaDro <sebastian.drost@hs-bochum.de>
2017-06-24 14:00:40 +0200

    implement text filter

--- sm4c-classifier/src/main/java/de/hsbo/fbg/sm4c/classifier/filter/StopWordRemover.java
+++ sm4c-classifier/src/main/java/de/hsbo/fbg/sm4c/classifier/filter/StopWordRemover.java
@@ -3,19 +3,68 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package de.hsbo.fbg.sm4c.tagging.filter;
+package de.hsbo.fbg.sm4c.classifier.filter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.logging.log4j.LogManager;
 
 /**
+ * Remover of stop words
  *
  * @author Sebastian Drost
  */
 public class StopWordRemover {
 
+    private static final org.apache.logging.log4j.Logger LOGGER = LogManager.getLogger(StopWordRemover.class);
+
+    public static final String DEFAULT_STOP_WORD_LIST = "german_stopwords_plain.txt";
+
+    private ArrayList<String> stopWordList;
 
     public StopWordRemover() {
+        stopWordList = new ArrayList();
+    }
+
+    public ArrayList<String> getStopWordList() {
+        return stopWordList;
+    }
+
+    /**
+     * Loads a List of default stop words from a given file
+     *
+     * @param listPath path starting from the classpath to the stop word list
+     */
+    public void loadStopWordList(String listPath) {
+        try (InputStream in = this.getClass().getClassLoader().getResourceAsStream(listPath)) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF8"));
+            String stopWord;
+            while ((stopWord = reader.readLine()) != null) {
+                if (!stopWord.startsWith(";")) {
+                    stopWordList.add(stopWord);
+                }
+            }
+        } catch (IOException ex) {
+            LOGGER.error("Could not load list of stop words", ex);
+        }
+    }
+
+    /**
+     * Removes the default stop words from a list of tokens
+     *
+     * @param tokens List of tokens the stop words will be removed of
+     * @return List of tokens without the default stop words
+     */
+    public List<String> removeStopWords(List<String> tokens) {
+        return tokens.stream().filter(t -> !stopWordList
+                .stream().anyMatch(sw -> sw.equalsIgnoreCase(t)))
+                .collect(Collectors.toList());
+
     }
 
     public String removeStopWords(String text, String[] stopWords) {
--- sm4c-classifier/src/main/java/de/hsbo/fbg/sm4c/classifier/filter/TextTokenizer.java
+++ sm4c-classifier/src/main/java/de/hsbo/fbg/sm4c/classifier/filter/TextTokenizer.java
@@ -3,14 +3,61 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package de.hsbo.fbg.sm4c.classifier.filter;
 
-package de.hsbo.fbg.sm4c.tagging.filter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+import org.apache.logging.log4j.LogManager;
 
 /**
+ * Tokenizer for texts
  *
  * @author Sebastian Drost
  */
-public class Tokenizer {
+public class TextTokenizer {
 
-    
+    private static final org.apache.logging.log4j.Logger LOGGER = LogManager.getLogger(TextTokenizer.class);
+
+    public static final String DEFAULT_TOKEN_MODEL = "de-token.bin";
+
+    private TokenizerModel tokenModel;
+    private Tokenizer tokenizer;
+
+    /**
+     * Trains the tokenizer with a given tokenizer model
+     *
+     * @param modelPath path starting from the classpath to the model
+     */
+    public void trainTokenModel(String modelPath) {
+        try (InputStream in = this.getClass().getClassLoader().getResourceAsStream(modelPath)) {
+            tokenModel = new TokenizerModel(in);
+            tokenizer = new TokenizerME(tokenModel);
+        } catch (IOException ex) {
+            LOGGER.error("Could not load tokenizer model", ex);
+        }
+    }
+
+    /**
+     * Tokenizes a given text
+     *
+     * @param text Text to tokenize
+     * @return List of tokens
+     */
+    public List<String> tokenize(String text) {
+        String[] tokens = null;
+        try {
+            tokens = tokenizer.tokenize(text);
+
+        } catch (NullPointerException ex) {
+            LOGGER.error("No tokenizer model has been trained", ex);
+        }
+        return Arrays.asList(tokens);
+    }
+
 }

7c820183b585214021f9f2e6327f05d13be507ca
SebaDro <sebastian.drost@hs-bochum.de>
2017-09-20 15:10:03 +0200

    remove arcgis geocoding and add feature persister

---
+++ sm4c-geotag/src/main/java/de/hsbo/fbg/sm4c/geotag/FeatureCreator.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.hsbo.fbg.sm4c.geotag;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import de.hsbo.fbg.sm4c.common.model.MessageDocument;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ *
+ * @author Sebastian Drost
+ */
+class FeatureCreator {
+
+    private static final Logger LOGGER = LogManager.getLogger(FeatureCreator.class);
+
+    private ObjectMapper mapper;
+
+    public FeatureCreator() {
+        mapper = new ObjectMapper();
+    }
+
+    public String createFeature(MessageDocument document) {
+        String result = null;
+        ArrayNode featuresNode = mapper.createArrayNode();
+        ObjectNode attributeNode = mapper.createObjectNode();
+        attributeNode.put("messageid", document.getId());
+//            attributeNode.put("collection", "test");
+        attributeNode.put("creation", document.getCreationTime().toString());
+        document.getLocations().forEach(l -> {
+            ObjectNode featureNode = mapper.createObjectNode();
+            ObjectNode geometryNode = mapper.createObjectNode();
+            geometryNode.put("x", l.getLongitude());
+            geometryNode.put("y", l.getLatitude());
+            featureNode.set("geometry", geometryNode);
+            featureNode.set("attributes", attributeNode);
+            featuresNode.add(featureNode);
+        });
+        try {
+            result = mapper.writeValueAsString(featuresNode);
+        } catch (JsonProcessingException ex) {
+            LOGGER.error("Could not encode feature", ex);
+        }
+        return result;
+    }
+}
---
+++ sm4c-geotag/src/main/java/de/hsbo/fbg/sm4c/geotag/FeatureServicePersister.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.hsbo.fbg.sm4c.geotag;
+
+import de.hsbo.fbg.sm4c.common.model.MessageDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ * @author Sebastian Drost
+ */
+public class FeatureServicePersister {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FeatureServicePersister.class);
+
+    private static final String F_PARAM = "f";
+    private static final String F_VALUE = "json";
+    private static final String ROLLBACK_PARAM = "rollbackOnFailure";
+    private static final String ROLLBACK_VALUE = "false";
+    private static final String TOKEN_PARAM = "token";
+    private static final String TOKEN_VALUE = "";
+    private static final String FEATURES_PARAM = "features";
+
+    private final HttpHeaders headers;
+    private final String featureServiceUrl;
+    private final FeatureCreator featureCreator;
+
+    public FeatureServicePersister(String featureServiceUrl) {
+        this.featureServiceUrl = featureServiceUrl;
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        featureCreator = new FeatureCreator();
+    }
+
+    public ResponseEntity<String> addDocumentAsFeature(MessageDocument document) {
+        String featureValue = featureCreator.createFeature(document);
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+        map.add(F_PARAM, F_VALUE);
+        map.add(ROLLBACK_PARAM, ROLLBACK_VALUE);
+        map.add(TOKEN_PARAM, TOKEN_VALUE);
+        map.add(FEATURES_PARAM, featureValue);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.postForEntity(featureServiceUrl, request, String.class);
+
+        return response;
+    }
+
+}
